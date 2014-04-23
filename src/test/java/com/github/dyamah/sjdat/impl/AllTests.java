21bb5ef06be2381837fe77fe318ce2fd8c13bc99
dyamah <dyama.h@gmail.com>
2014-04-23 23:39:33 +0900

    djunitのためにテストスイートを追加

---
+++ src/test/java/com/github/dyamah/sjdat/impl/AllTests.java
@@ -0,0 +1,11 @@
+package com.github.dyamah.sjdat.impl;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ DoubleArrayTrieImplTest.class, NodeTest.class })
+public class AllTests {
+
+}
