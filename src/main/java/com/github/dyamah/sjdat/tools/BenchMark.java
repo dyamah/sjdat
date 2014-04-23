bf46b5d2cad25054885e1f6592498fb1a5f74513
dyamah <dyama.h@gmail.com>
2014-04-24 01:11:22 +0900

    ベンチマーククラスの追加 ツール名の変更

---
+++ src/main/java/com/github/dyamah/sjdat/tools/BenchMark.java
@@ -0,0 +1,85 @@
+package com.github.dyamah.sjdat.tools;
+
+import java.io.PrintStream;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryPoolMXBean;
+import java.lang.management.MemoryType;
+import java.lang.management.MemoryUsage;
+import java.text.DecimalFormat;
+
+public class BenchMark {
+    private static final DecimalFormat DFORMAT = new DecimalFormat(".#");
+    private static final String HEADER = "                \t    time [ms]\t  memory [kb]";
+    private long peek_memory_;
+    private long total_time_ ;
+    private long start_memory_ ;
+    private long stop_memory_ ;
+    private long start_time_;
+    private long stop_time_;
+    private PrintStream out_ ;
+
+    private long getPeakMemory() {
+        long peak = 0;
+        for (MemoryPoolMXBean mbean : ManagementFactory
+                .getMemoryPoolMXBeans()) {
+            if (mbean.getType() != MemoryType.HEAP)
+                continue;
+            if (!mbean.isValid())
+                continue;
+            MemoryUsage usage = mbean.getPeakUsage();
+            peak += usage.getUsed();
+        }
+        return peak;
+    }
+
+    public BenchMark(){
+        peek_memory_ = 0;
+        total_time_  = 0;
+        start_time_  = 0;
+        stop_time_   = 0;
+
+        out_ = System.err;
+    }
+
+    public void showHeader(){
+        if (out_ == null)
+            return ;
+        out_.println(HEADER);
+    }
+    public void showTotal(){
+        if (out_ == null)
+            return ;
+        out_.printf("%16s", "[TOTAL]");
+        out_.printf("\t%13s", total_time_);
+        out_.printf("\t%13s", DFORMAT.format((double)peek_memory_ / 1024));
+        out_.println();
+
+    }
+
+    public void start(String message){
+        start_memory_ = getPeakMemory();
+        start_time_ = System.currentTimeMillis();
+        if (out_ == null)
+            return ;
+        String msg = message ;
+        if (message.length() > 16)
+            msg = message.substring(0, 16);
+        out_.printf("%16s", msg);
+    }
+
+    public void stop(){
+        stop_time_ = System.currentTimeMillis();
+        stop_memory_ = getPeakMemory();
+        if (stop_memory_ > peek_memory_)
+            peek_memory_ = stop_memory_ ;
+
+        total_time_ += (stop_time_ - start_time_);
+        if (out_ == null)
+            return ;
+
+        out_.printf("\t%13s",(stop_time_ - start_time_));
+        out_.printf("\t%13s", DFORMAT.format((double)(stop_memory_ - start_memory_) / 1024));
+        out_.println();
+    }
+
+}
--- src/main/java/com/github/dyamah/sjdat/tools/mktrie.java
+++ src/main/java/com/github/dyamah/sjdat/tools/mktrie.java
@@ -14,26 +14,9 @@ import com.github.dyamah.sjdat.Trie;
 import com.github.dyamah.sjdat.TrieBuilder;
 import com.github.dyamah.sjdat.impl.DoubleArrayTrieImpl;
 
-public class CLT {
-    static class StopWatch {
-        long start_ ;
-        long stop_ ;
-        StopWatch(){
-           start_ = 0;
-           stop_ = 0;
-        }
-        void start(){
-            start_ = System.currentTimeMillis();
-        }
+public class mktrie {
 
-        void stop(){
-            stop_ =  System.currentTimeMillis() ;
-        }
-
-        void show(String arg){
-            System.err.println(arg + ": "  + (stop_ - start_) + " [ms]");
-        }
-    }
+    static BenchMark bm = new BenchMark();
 
     @SuppressWarnings("resource")
     static List<String> readKeys(File file){
@@ -42,14 +25,14 @@ public class CLT {
         try {
             reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
             keys = new ArrayList<String>();
-            StopWatch sw = new StopWatch();
-            sw.start();
+
+            bm.start("[Read]");
             String line = null;
             while((line = reader.readLine()) != null)
                 keys.add(line);
 
-            sw.stop();
-            sw.show("Reading time");
+            bm.stop();
+
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
@@ -60,39 +43,37 @@ public class CLT {
 
     static Trie buildTrie(List<String> keys){
         TrieBuilder builder = DoubleArrayTrieImpl.createBuilder();
-        StopWatch sw = new StopWatch();
-        sw.start();
+
+        bm.start("[Build]");
         Trie trie = builder.build(keys);
-        sw.stop();
-        sw.show("Build time");
-        System.err.println("#keys: " + trie.numberOfKeys());
-        System.err.println("#nodes: " + trie.numberOfNodes());
-        System.err.println("#free: " + trie.numberOfFreeNodes());
+        bm.stop();
+
+
         return trie;
     }
     static void checkTrie(Trie trie, List<String> keys){
         Map<Integer, String> map = new TreeMap<Integer,String>();
 
-        boolean error = false;
+        bm.start("[Check]");
         for(String key: keys){
             int id = trie.lookup(key);
 
             if (map.containsKey(id) || id < 0){
-                error = true;
                 System.err.println("Duplication: " + id + ":" + key);
             }
         }
-        if (map.size() == keys.size())
-            System.err.println("There is no error.");
+
+        bm.stop();
     }
 
     static void save(Trie trie, File file){
+
         try {
-            StopWatch sw = new StopWatch();
-            sw.start();
+
+            bm.start("[Save]");
             trie.save(file);
-            sw.stop();
-            sw.show("Saving time");
+            bm.stop();
+
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
@@ -104,14 +85,20 @@ public class CLT {
      */
     public static void main(String[] args) {
         if (args.length < 2){
-            System.err.println("usage: com.github.dyamah.sjdat.tools.CLT input output");
+            System.err.println("usage: com.github.dyamah.sjdat.tools.mktrie input output");
             System.exit(1);
         }
+        bm.showHeader();
         List<String> keys = readKeys(new File(args[0]));
         Trie trie = buildTrie(keys);
         checkTrie(trie, keys);
         save(trie, new File(args[1]));
-
+        bm.showTotal();
+        System.err.println();
+        System.err.println("Trie Info.");
+        System.err.println("\t#keys      : " + trie.numberOfKeys());
+        System.err.println("\t#nodes     : " + trie.numberOfNodes());
+        System.err.println("\t#free nodes: " + trie.numberOfFreeNodes());
     }
 
 }
